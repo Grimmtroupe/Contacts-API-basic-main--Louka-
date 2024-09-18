1cab2299de2fe9316546d82f4a2a069a74e711ad
Louka <95499931+Grimmtroupe@users.noreply.github.com>
2024-09-18 16:19:11 -0400

    Initial commit

---
+++ Contacts-Manager-SPA/js/siteUI.js
@@ -0,0 +1,243 @@
+//<span class="cmdIcon fa-solid fa-ellipsis-vertical"></span>
+let contentScrollPosition = 0;
+Init_UI();
+
+function Init_UI() {
+    renderBookmarks();
+    $('#createBookmark').on("click", async function () {
+        saveContentScrollPosition();
+        renderCreateBookmarkForm();
+    });
+    $('#abort').on("click", async function () {
+        renderBookmarks();
+    });
+    $('#aboutCmd').on("click", function () {
+        renderAbout();
+    });
+}
+
+function renderAbout() {
+    saveContentScrollPosition();
+    eraseContent();
+    $("#createBookmark").hide();
+    $("#abort").show();
+    $("#actionTitle").text("À propos...");
+    $("#content").append(
+        $(`
+            <div class="aboutContainer">
+                <h2>Gestionnaire de Favoris</h2>
+                <hr>
+                <p>
+                    Petite application permettant d'ajouter,modifier ou supprimer des sites mis en Favoris 
+                </p>
+                <p>
+                    Auteur: Louka Janelle ( avec la partie de programme donner par Nicolas Chourot)
+                </p>
+                <p>
+                    Collège Lionel-Groulx, automne 2024
+                </p>
+            </div>
+        `))
+}
+async function renderBookmarks() {
+    showWaitingGif();
+    $("#actionTitle").text("Liste des Favoris");
+    $("#createBookmark").show();
+    $("#abort").hide();
+    let bookmarks = await API_GetBookmarks();
+    eraseContent();
+    if (bookmarks !== null) {
+        bookmarks.forEach(bookmark => {
+            $("#content").append(renderBookmark(bookmark));
+        });
+        restoreContentScrollPosition();
+        // Attached click events on command icons
+        $(".editCmd").on("click", function () {
+            saveContentScrollPosition();
+            renderEditBookmarkForm(parseInt($(this).attr("editbookmarkId")));
+        });
+        $(".deleteCmd").on("click", function () {
+            saveContentScrollPosition();
+            renderDeleteBookmarkForm(parseInt($(this).attr("deletebookmarkId")));
+        });
+        $(".bookmarkRow").on("click", function (e) { e.preventDefault(); })
+    } else {
+        renderError("Service introuvable");
+    }
+}
+function showWaitingGif() {
+    $("#content").empty();
+    $("#content").append($("<div class='waitingGifcontainer'><img class='waitingGif' src='Loading_icon.gif' /></div>'"));
+}
+function eraseContent() {
+    $("#content").empty();
+}
+function saveContentScrollPosition() {
+    contentScrollPosition = $("#content")[0].scrollTop;
+}
+function restoreContentScrollPosition() {
+    $("#content")[0].scrollTop = contentScrollPosition;
+}
+function renderError(message) {
+    eraseContent();
+    $("#content").append(
+        $(`
+            <div class="errorContainer">
+                ${message}
+            </div>
+        `)
+    );
+}
+function renderCreateBookmarkForm() {
+    renderBookmarkForm();
+}
+async function renderEditBookmarkForm(id) {
+    showWaitingGif();
+    let bookmark = await API_GetBookmark(id);
+    if (bookmark !== null)
+        renderBookmarkForm(bookmark);
+    else
+        renderError("bookmark introuvable!");
+}
+async function renderDeleteBookmarkForm(id) {
+    showWaitingGif();
+    $("#createBookmark").hide();
+    $("#abort").show();
+    $("#actionTitle").text("Retrait");
+    let bookmark = await API_GetBookmark(id);
+    eraseContent();
+    if (bookmark !== null) {
+        $("#content").append(`
+        <div class="bookmarkdeleteForm">
+            <h4>Effacer le favori suivant?</h4>
+            <br>
+            <div class="bookmarkRow" bookmark_id=${bookmark.Id}">
+                <div class="bookmarkContainer">
+                    <div class="bookmarkLayout">
+                        <div class="bookmarkName">${bookmark.Title}</div>
+                        <div class="bookmarkPhone">${bookmark.Url}</div>
+                        <div class="bookmarkEmail">${bookmark.Category}</div>
+                    </div>
+                </div>  
+            </div>   
+            <br>
+            <input type="button" value="Effacer" id="deletebookmark" class="btn btn-primary">
+            <input type="button" value="Annuler" id="cancel" class="btn btn-secondary">
+        </div>    
+        `);
+        $('#deletebookmark').on("click", async function () {
+            showWaitingGif();
+            let result = await API_DeleteBookmark(bookmark.Id);
+            if (result)
+                renderBookmarks();
+            else
+                renderError("Une erreur est survenue!");
+        });
+        $('#cancel').on("click", function () {
+            renderBookmarks();
+        });
+    } else {
+        renderError("bookmark introuvable!");
+    }
+}
+function newBookmark() {
+    bookmark = {};
+    bookmark.Id = 0;
+    bookmark.title = "";
+    bookmark.url = "";
+    bookmark.category = "";
+    return bookmark;
+}
+function renderBookmarkForm(bookmark = null) {
+    $("#createBookmark").hide();
+    $("#abort").show();
+    eraseContent();
+    let create = bookmark == null;
+    if (create) bookmark = newBookmark();
+    $("#actionTitle").text(create ? "Création" : "Modification");
+    $("#content").append(`
+        <form class="form" id="bookmarkForm">
+            <input type="hidden" name="Id" value="${bookmark.Id}"/>
+
+            <label for="Name" class="form-label">Titre </label>
+            <input 
+                class="form-control Alpha"
+                name="Name" 
+                id="Name" 
+                placeholder="Nom"
+                required
+                RequireMessage="Veuillez entrer le Titre du site"
+                InvalidMessage="Le nom comporte un caractère illégal" 
+                value="${bookmark.Title}"
+            />
+            <label for="Phone" class="form-label">Url </label>
+            <input
+                class="form-control URL"
+                name="Phone"
+                id="Phone"
+                placeholder="https:..."
+                required
+                RequireMessage="Veuillez entrer l'url du site" 
+                InvalidMessage="Veuillez entrer un url valide"
+                value="${bookmark.Url}" 
+            />
+            <label for="Email" class="form-label">Catégorie </label>
+            <input 
+                class="form-control Alpha"
+                name="Catégorie"
+                id="Catégorie"
+                placeholder="ex: moteur de recherche"
+                required
+                RequireMessage="Veuillez entrer la catégorie du site" 
+                InvalidMessage="Veuillez entrer une catégorie valide"
+                value="${bookmark.Category}"
+            />
+            <hr>
+            <input type="submit" value="Enregistrer" id="savebookmark" class="btn btn-primary">
+            <input type="button" value="Annuler" id="cancel" class="btn btn-secondary">
+        </form>
+    `);
+    initFormValidation();
+    $('#bookmarkForm').on("submit", async function (event) {
+        event.preventDefault();
+        let bookmark = getFormData($("#bookmarkForm"));
+        bookmark.Id = parseInt(bookmark.Id);
+        showWaitingGif();
+        let result = await API_SaveBookmark(bookmark, create);
+        if (result)
+            renderBookmarks();
+        else
+            renderError("Une erreur est survenue!");
+    });
+    $('#cancel').on("click", function () {
+        renderBookmarks();
+    });
+}
+
+function getFormData($form) {
+    const removeTag = new RegExp("(<[a-zA-Z0-9]+>)|(</[a-zA-Z0-9]+>)", "g");
+    var jsonObject = {};
+    $.each($form.serializeArray(), (index, control) => {
+        jsonObject[control.name] = control.value.replace(removeTag, "");
+    });
+    return jsonObject;
+}
+
+function renderBookmark(bookmark) {
+    return $(`
+     <div class="bookmarkRow" bookmark_id=${bookmark.Id}">
+        <div class="bookmarkContainer noselect">
+            <div class="bookmarkLayout">
+                <img src="bookmark-star.svg" class="appLogo" alt="" title="Gestionnaire de Favoris">
+                <span class="bookmarkTitle">${bookmark.Title}</span>
+                </br>
+                <span class="bookmarkCategory">${bookmark.Category}</span>
+            </div>
+            <div class="bookmarkCommandPanel">
+                <span class="editCmd cmdIcon fa fa-pencil" editbookmarkId="${bookmark.Id}" title="Modifier ${bookmark.title}"></span>
+                <span class="deleteCmd cmdIcon fa fa-trash" deletebookmarkId="${bookmark.Id}" title="Effacer ${bookmark.title}"></span>
+            </div>
+        </div>
+    </div>           
+    `);
+}
---
+++ server.js
@@ -0,0 +1,189 @@
+import { createServer } from 'http';
+import fs from 'fs';
+
+function allowAllAnonymousAccess(res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Access-Control-Expose-Headers', '*');
+}
+function accessControlConfig(req, res) {
+    if (req.headers['sec-fetch-mode'] == 'cors') {
+        allowAllAnonymousAccess(res);
+        console.log("Client browser CORS check request");
+    }
+}
+function CORS_Preflight(req, res) {
+    if (req.method === 'OPTIONS') {
+        res.end();
+        console.log("Client browser CORS preflight check request");
+        return true;
+    }
+    return false;
+}
+function extract_Id_From_Request(req) {
+    // .../api/ressources/id
+    let parts = req.url.split('/');
+    return parseInt(parts[parts.length - 1]);
+}
+function valideBookmark(bookmark){
+    if (!('Title' in bookmark)) return 'Title is missing';
+    if (!('Url' in bookmark)) return 'Url is missing';
+    if (!('Category' in bookmark)) return 'Category is missing';
+    return '';
+}
+async function handleBookmarkServiceRequest(req, res) {
+    if (req.url.includes("/api/Bookmarks"))
+    {
+        const bookmarksFilePath = "./bookmarks.json";
+        let bookmarksJSON = fs.readFileSync(bookmarksFilePath);
+        let bookmarks = JSON.parse(bookmarksJSON);
+        let validStatus = '';
+        let id = extract_Id_From_Request(req);
+        switch (req.method) {
+            case 'GET':
+                if (isNaN(id)) {
+                    res.writeHead(200, { 'content-type': 'application/json' });
+                    res.end(bookmarksJSON);
+                } 
+                else {
+                    let found = false;
+                    for (let bookmark of bookmarks) {
+                        if (bookmark.Id === id) {
+                            found = true;
+                            res.writeHead(200, { 'content-type': 'application/json' });
+                            res.end(JSON.stringify(bookmark));
+                            break;
+                        }
+                    }
+                    if (!found) {
+                        res.writeHead(404);
+                        res.end(`Erreur : l ${id} du bookmark n existe pas`);
+                    }
+                }
+                break;
+                case 'POST':
+                    let newBookmark = await getPayload(req);
+                    validStatus = validateBookmark(newBookmark);
+                    if (validStatus == '') {
+                        let maxId = 0;
+                        bookmarks.forEach(bookmark => {
+                            if (bookmark.Id > maxId)
+                                maxId = bookmark.Id;
+                        });
+                        newBookmark.Id = maxId + 1;
+                        bookmarks.push(newBookmark);
+                        fs.writeFileSync(bookmarksFilePath, JSON.stringify(bookmarks));
+                        res.writeHead(201, { 'content-type': 'application/json' });
+                        res.end(JSON.stringify(newBookmark));
+                    } else {
+                        res.writeHead(400);
+                        res.end(`Error: ${validStatus}`);
+                    }
+                    break;
+            case 'PUT':
+                let modifiedBookmark = await getPayload(req);
+                validStatus = valideBookmark(modifiedBookmark);
+                if (validStatus == '') {
+                    if (!isNaN(id)) {
+                        if (!('Id' in modifiedBookmark)) modifiedBookmark.Id = id;
+                        if (modifiedBookmark.Id == id) {
+                            let storedBookmark = null;
+                            for (let bookmark of bookmarks) {
+                                if (bookmark.Id === id) {
+                                    storedbookmark = bookmark;
+                                    break;
+                                }
+                            }
+                            if (storedBookmark != null) {
+                                storedBookmark.Title = storedBookmark.Title;
+                                storedBookmark.Url = storedBookmark.Url;
+                                storedBookmark.Category = storedBookmark.Category;
+                                fs.writeFileSync(bookmarksFilePath, JSON.stringify(bookmarks));
+                                res.writeHead(200);
+                                res.end();
+                            } else {
+                                res.writeHead(404);
+                                res.end(`Erreur : l ${id} du bookmark n existe pas`);
+                            }
+                        } else {
+                            res.writeHead(409);
+                            res.end(`Error: Conflict of id`);
+                        }
+                    } else {
+                        res.writeHead(400);
+                        res.end("Erreur : pour modifier un favoris vous devez donner un id .");
+                    }
+                } else {
+                    res.writeHead(400);
+                    res.end(`Error: ${validStatus}`);
+                }
+                break;
+            case 'DELETE':
+                if (!isNaN(id)) {
+                    let index = 0;
+                    let oneDeleted = false;
+                    for (let bookmark of bookmarks) {
+                        if (bookmark.Id === id) {
+                            bookmarks.splice(index, 1);
+                            fs.writeFileSync(bookmarksFilePath, JSON.stringify(bookmarks));
+                            oneDeleted = true;
+                            break;
+                        }
+                        index++;
+                    }
+                    if (oneDeleted) {
+                        res.writeHead(204); // success no content
+                        res.end();
+                    } else {
+                        res.writeHead(404);
+                        res.end(`Erreur : l ${id} du bookmark n existe pas`);
+                    }
+                } else {
+                    res.writeHead(400);
+                    res.end("Erreur : pour effacer un favoris vous devez donner un id .");
+                }
+                break;
+            case 'PATCH':
+                res.writeHead(501);
+                res.end("Error: The endpoint PATCH api/bookmarks is not implemented.");
+                break;
+        }
+        return true;
+    }
+    return false;
+}
+
+async function handleRequest(req, res) {
+    if (! await handleBookmarkServiceRequest(req, res))
+        if (! await handleBookmarkServiceRequest(req, res))
+            return false;
+    return true;
+}
+
+function getPayload(req) {
+    return new Promise(resolve => {
+        let body = [];
+        req.on('data', chunk => { body += chunk; });
+        req.on('end', () => {
+            if (body.length > 0)
+                if (req.headers['content-type'] == "application/json")
+                    try { resolve(JSON.parse(body)); }
+                    catch (error) { console.log(error); }
+            resolve(null);
+        });
+    })
+}
+
+const server = createServer(async (req, res) => {
+    console.log(req.method, req.url);
+    accessControlConfig(req, res);
+    if (!CORS_Preflight(req, res))
+        if (!await handleRequest(req, res)) {
+            res.writeHead(404);
+            res.end();
+        }
+});
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
